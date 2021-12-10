25c0e756fbe40213f165552277004b6811083355
Hana <momenthana@kakao.com>
2021-12-10 15:57:28 +0900

    Update client information

--- src/graphql/queries/app/resolver.js
+++ src/graphql/queries/app/resolver.js
@@ -6,9 +6,13 @@ import App from "@/models/app"
 export const app = async (_, args, { payload }) => {
   if (!payload) throw new AuthenticationError()
 
-  const apps = await App.find({
+  const filter = {
     id: MUUID.from(payload.sub),
-  })
+  }
+
+  if (args.input?.clientId) filter.clientId = MUUID.from(args.input?.clientId)
+
+  const apps = await App.find(filter)
 
   apps.forEach((app) => {
     app.clientId = MUUID.from(app.clientId).toString()
