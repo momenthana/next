4307fa39ddcb74925c47980b6ed6caa3de3230ca
Hana <momenthana@kakao.com>
2022-01-05 10:25:35 +0900

    Create vue devServer option

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
+let devServer
+
+if (process.env.NODE_ENV !== "production") {
+  devServer = {
+    host: process.env.HOST,
+    port: process.env.PORT,
+    https: {
+      key: require("fs").readFileSync("env/privkey.pem"),
+      cert: require("fs").readFileSync("env/fullchain.pem"),
+    },
+  }
+}
+
 module.exports = {
+  devServer,
   transpileDependencies: ["vuetify"],
   configureWebpack: {
     module: {
