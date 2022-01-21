363da326444e6b8b32f01fa6d1b20120ca43c49b
Hana <momenthana@kakao.com>
2022-01-21 13:04:41 +0900

    fix: alice env findById

--- src/lib/alice.js
+++ src/lib/alice.js
@@ -3,9 +3,7 @@ import { env } from 'process'
 import Env from '@/models/env'
 
 export const init = async () => {
-  const data = await Env.findOne({
-    NAME: env.NAME
-  }).lean()
+  const data = await Env.findById(env.ID).lean()
 
   Object.assign(env, data)
 }
