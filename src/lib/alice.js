a87657a96dc3cbac462562069a066f24b2326b9b
Hana <momenthana@kakao.com>
2022-02-17 15:05:44 +0900

    feat: servername aggregate envs

--- src/lib/alice.js
+++ src/lib/alice.js
@@ -3,7 +3,23 @@ import { env } from 'process'
 import Env from '@/models/env'
 
 export const init = async () => {
-  const data = await Env.findById(env.ID).lean()
+  const data = await Env.aggregate([
+    { $match: { NAME: env.NAME } },
+    {
+      $lookup: {
+        from: 'certs',
+        localField: 'certId',
+        foreignField: '_id',
+        as: 'cert'
+      }
+    },
+    {
+      $replaceRoot: {
+        newRoot: { $mergeObjects: [{ $arrayElemAt: ['$cert', 0] }, '$$ROOT'] }
+      }
+    },
+    { $project: { _id: 0, certId: 0, cert: 0 } }
+  ])
 
-  Object.assign(env, data)
+  Object.assign(env, data.shift())
 }
